6b4e60e20594c9620b889d20f6e5edba2eb33f1b
Jirayus Chompen <kurakiriyami@gmail.com>
2023-09-14 15:36:05 +0700

    Added foo to the bar

---
+++ phpExercise/Exercise-4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise-4</title>
+</head>
+<body>
+    <?php
+
+        function test($s) {
+            $crt_aa = 0;
+            $i = 0; 
+            while ($i < (strlen($s) - 1)) {
+                if (substr($s, $i, 2) == "aa") {
+                    $crt_aa++;   
+                    $i += 1;
+                } else {
+                    $i++;
+                }
+            }
+            return $crt_aa;
+        }
+    echo test("bbaaccaag")."\n";
+    echo test("jjkiaaasew")."\n";
+    echo test("JSaaakoiaa")."\n";
+    
+    
+    ?>
+</body>
+</html>
---
+++ phpExercise/ex01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        #this is my first function
+        function calSalary()
+        {
+            $mont = 12*65000;
+            echo "this is my salary".$mont."bath";
+        }
+        #calling function 
+        calSalary();
+
+    ?>
+</body>
+</html>
